fa826072033fbe5eb36b71733f58959ddb16d30e
Carlos Aguirre <craguirre952@gmail.com>
2021-07-12 21:25:34 -0400

    add calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+/* global Vue */
+
+var app = new Vue({
+  el: "#app",
+  data: function() {
+    return {
+      message: "Welcome to Internet Calculator",
+      number1: 0,
+      number2: 0,
+      number3: 0,
+      sumTotal: null
+      productTotal: null
+    };
+  },
+  methods: {
+    sum: function() {
+      console.log(this.text);
+      this.sumTotal = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+    },
+    product: function() {
+      console.log(this.text);
+      this.productTotal = parseInt(this.number1) * parseInt(this.number2) * parseInt(this.number3);
+    }
+  }
+});
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* global Vue */
+
 var app = new Vue({
   el: "#app",
   data: function() {
