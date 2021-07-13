29340dc80e9697353034fb48daddaa8cdb0d8d74
Carlos Aguirre <craguirre952@gmail.com>
2021-07-12 21:55:01 -0400

    add toggle, CSS and grey out text

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ var app = new Vue({
     return {
       message: "Hello from JavaScript!",
       subMessage: "Hi again!",
-      thirdMessage: "Hi."
+      thirdMessage: "Hi.",
+      showHiddenInfo: false,
+      greyOutInfo: false
     };
   },
   methods: {
     goodbye: function () {
       console.log("Goodbye!");
       this.subMessage = "Goodbye!";
+    },
+    toggle: function () {
+      console.log('in toggle');
+      this.showHiddenInfo = ! this.showHiddenInfo;
+    },
+    greyOut: function () {
+      console.log('greying out text');
+      this.greyOutInfo = ! this.greyOutInfo;
     }
   }
 });
